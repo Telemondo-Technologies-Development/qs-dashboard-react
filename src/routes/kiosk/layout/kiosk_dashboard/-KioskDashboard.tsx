4efa070481cf964d09029abacced6807c5e61bd3
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-05-15 22:54:53 +0800

    feat: installed zustand, handled state mgmt for counter selection
    In this commit I:
    
    - installed zustand
    - created a zustand store for handling state mgmt of counter selection and deselection
    - implemented said zustand store in KioskDashboard

--- src/routes/kiosk/layout/kiosk_dashboard/-KioskDashboard.tsx
+++ src/routes/kiosk/layout/kiosk_dashboard/-KioskDashboard.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "@tanstack/react-router";
-import { useState } from "react";
+import { useEffect } from "react";
 import KioskDialog from "../kiosk_dialog/-KioskDialog";
+import {
+  useSelectedCounterStore,
+  counterTypes,
+} from "../../state_management/-CounterType";
 
 export default function KioskDashboard() {
-  const [selectedButton, setSelectedbutton] = useState("");
+  const selectedCounter = useSelectedCounterStore(
+    (state) => state.selectedCounter
+  );
+  const setSelectedCounterType = useSelectedCounterStore(
+    (state) => state.setSelectedCounter
+  );
+  const resetSelectedCounter = useSelectedCounterStore(
+    (state) => state.resetSelectedCounter
+  );
+
+  useEffect(() => {
+    console.log(selectedCounter);
+  }, [selectedCounter]);
+
   return (
     <div className="flex flex-col items-center justify-center flex-1 bg-white font-poppins text-main_primary">
       <h1 className="mb-3 text-5xl font-bold">Queue</h1>
@@ -12,67 +29,32 @@ export default function KioskDashboard() {
         Select one of the following to generate a QR queue ticket.
       </p>
       <div className="flex flex-row gap-10 ">
-        <button
-          className={`flex justify-center flex-col items-center font-bold text-xl gap-4 `}
-          onClick={() => setSelectedbutton("accounts")}
-        >
-          <img
-            className={`size-56 ${selectedButton === "" ? "brightness-100" : `${selectedButton === "accounts" ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
-            src="/kiosk-images/accounts.png"
-            alt="accounts"
-          />
-          <p className="text-lg">Accounts</p>
-        </button>
-        <button
-          className="flex flex-col items-center justify-center gap-5 text-xl font-bold "
-          onClick={() => setSelectedbutton("Deposits and Withdrawals")}
-        >
-          <img
-            className={`size-56 ${selectedButton === "" ? "brightness-100" : `${selectedButton === "Deposits and Withdrawals" ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
-            src="/kiosk-images/deposits-and-withdrawal.png"
-            alt="Deposits and Withdrawals"
-          />
-          <p className="text-lg">Deposits and Withdrawals</p>
-        </button>
-        <button
-          className="flex flex-col items-center justify-center gap-5 text-xl font-bold"
-          onClick={() => setSelectedbutton("Loans and Mortgages")}
-        >
-          <img
-            className={`size-56 ${selectedButton === "" ? "brightness-100" : `${selectedButton === "Loans and Mortgages" ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
-            src="/kiosk-images/loans-and-mortgages.png"
-            alt="Loans and Mortgages"
-          />
-          <p className="text-lg">Loans and Mortgages</p>
-        </button>
-        <button
-          className="flex flex-col items-center justify-center gap-5 text-xl font-bold"
-          onClick={() => setSelectedbutton("Payements and Transfer")}
-        >
-          <img
-            className={`size-56 ${selectedButton === "" ? "brightness-100" : `${selectedButton === "Payements and Transfer" ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
-            src="/kiosk-images/payments-and-transfers.png"
-            alt="Payements and Transfer"
-          />
-          <p className="text-lg">Payments and Transfer</p>
-        </button>
-        <button
-          className={
-            "flex justify-center flex-col items-center font-bold text-xl gap-5 "
-          }
-          onClick={() => setSelectedbutton("Customer Service")}
-        >
-          <img
-            className={`size-56 ${selectedButton === "" ? "brightness-100" : `${selectedButton === "Customer Service" ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
-            src="/kiosk-images/customers-service.png"
-            alt="Customer Service"
-          />
-          <p className="text-lg">Customer Service</p>
-        </button>
+        {counterTypes.map((counterType) => {
+          return (
+            <button
+              key={counterType.counterID}
+              className={`flex justify-center flex-col items-center font-bold text-xl gap-4 `}
+              onClick={() => {
+                if (selectedCounter.counterID === counterType.counterID) {
+                  resetSelectedCounter();
+                } else {
+                  setSelectedCounterType(counterType.counterID);
+                }
+              }}
+            >
+              <img
+                className={`size-56 ${!selectedCounter.counterID ? "brightness-100" : `${selectedCounter.counterID === counterType.counterID ? "brightness-100 border-4 border-main_primary rounded-2xl " : "brightness-50 transition duration-300"}`} `}
+                src={counterType.counterImage}
+                alt={counterType.counterName}
+              />
+              <p className="text-lg">{counterType.counterName}</p>
+            </button>
+          );
+        })}
       </div>
       <div className="flex gap-48 mt-16 text-lg">
         {/* placeholder lang ni kay need og button pang open sa dialog */}
-        <KioskDialog/>
+        <KioskDialog />
         {/* <Link to="/">
           <Button className="w-64 h-16 text-lg bg-main_primary">
             Regular Citizen
