b14c890b68bf6c3a950fb2c1042c28ab0545e6a8
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-05-16 23:37:39 +0800

    feat: changed kiosk from lazy to normal file route
    In this commit I:
    
    - changed kiosk route.lazy.tsx to route.tsx
    - removed pending component in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,5 @@
 import { Navigate, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  pendingComponent: () => (
-    <div className="grid h-screen text-black place-items-center">
-      LOADING...
-    </div>
-  ),
-  component: () => <Navigate to="/kiosk" />,
+  component: () => <Navigate to="/kiosk" replace={true}/>,
 });
