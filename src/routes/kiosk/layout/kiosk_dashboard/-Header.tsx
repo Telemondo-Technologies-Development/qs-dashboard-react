ac8cbd1ca4f1857fa3fc6b89853637b6f6bf1b5c
rjamesmoreno <rendelljames20001@gmail.com>
2024-05-16 13:49:58 +0800

    feat: enhanced header and footer layout

--- src/routes/kiosk/layout/kiosk_dashboard/-Header.tsx
+++ src/routes/kiosk/layout/kiosk_dashboard/-Header.tsx
@@ -1,5 +1,5 @@
+import formatDateAndTime from "@/utils/dateFormatter";
 import { useEffect, useState } from "react";
-import KBQSLogo from "/kiosk-logo.png";
 
 export default function Header() {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -8,38 +8,20 @@ export default function Header() {
     setCurrentTime(new Date());
   };
 
-  const getCurrentDateTime = () => {
-    const currentDate = new Date();
-    const optionsDate: Intl.DateTimeFormatOptions = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    const optionsTime: Intl.DateTimeFormatOptions = {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    };
-    const formattedDate = currentDate.toLocaleString("en-US", optionsDate);
-    const formattedTime = currentDate.toLocaleString("en-US", optionsTime);
-    return { date: formattedDate, time: formattedTime };
-  };
-
   useEffect(() => {
     const intervalId = setInterval(updateCurrentTime, 1000);
     return () => clearInterval(intervalId);
-  }, [currentTime]);
+  }, []);
 
-  const { date, time } = getCurrentDateTime();
+  const { date, time } = formatDateAndTime(currentTime);
   return (
     <header className="w-screen bg-[#2F4E6E] text-white py-3 px-12">
       <div className="flex justify-between">
         <div className="flex ">
           <img
-            src={KBQSLogo}
+            src="/kiosk-logo.png"
             alt="kbqs logo"
-            className="size-[60px] self-center"
+            className="size-[70px] self-center"
           />
           <div className="self-center pl-5">
             <h1 className="text-2xl font-bold">KIOSK</h1>
