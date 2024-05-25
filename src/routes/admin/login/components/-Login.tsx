1a37cf769a45cd6ba133e3c498294ba6a6c9a0cc
Rendell James Moreno <rjm.moreno2001@gmail.com>
2024-05-25 23:45:45 +0800

    feat: enhanced login form
    in this commit i:
    - created zod schema for the login
    - used react hook forms
    - used shadcn Forms for login

--- src/routes/admin/login/components/-Login.tsx
+++ src/routes/admin/login/components/-Login.tsx
@@ -1,24 +1,108 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { IconLoader2 } from "@tabler/icons-react";
+
+const usernameTest = "admin";
+const passwordTest = "123";
+
+const loginSchema = z
+  .object({
+    username: z.string(),
+    password: z.string(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.username !== usernameTest) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Please enter a valid username",
+        path: ["username"],
+      });
+    }
+    if (data.password !== passwordTest) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Incorrect password",
+        path: ["password"],
+      });
+    }
+  });
+
 export default function Login() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(values);
+    form.reset();
+  }
+
   return (
-    <div className="flex flex-col bg-main_white rounded-lg place-items-center gap-10 px-[70px] pt-[50px] pb-[100px] font-poppins">
+    <div className="flex flex-col bg-main_white rounded-lg place-items-center gap-10 px-[120px] pt-[50px] pb-[100px] font-poppins">
       <img src="/kiosk-logo.png" alt="kbqs logo" className="size-[10rem]" />
       <div className="flex flex-col">
         <h1 className="text-xl font-bold text-center">ADMIN PANEL</h1>
         <p className="text-center">Login to access your admin account</p>
       </div>
-      <form className="flex flex-col place-items-center gap-5 w-[100%]" action="">
-        <input
-          placeholder="username"
-          type="text"
-          className="rounded-lg border-black border-[1px] w-[100%] h-[30px]"
-        />
-        <input
-          placeholder="password"
-          type="text"
-          className="rounded-lg border-black border-[1px] w-[100%] h-[30px]"
-        />
-      </form>
-      <button className="px-[70px] py-2 bg-main_primary text-main_white rounded-md">Login</button>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col place-items-center gap-10 w-[100%]"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="w-[130%] h-[30px]">
+                <FormControl>
+                  <Input placeholder="username" {...field} type="username" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="w-[130%] h-[30px]">
+                <FormControl>
+                  <Input placeholder="password" {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <button
+            type="submit"
+            disabled={form.formState.isSubmitting}
+            className="flex items-center justify-center w-[50%] py-2 bg-main_primary hover:bg-main_primary/90 text-main_white rounded-md "
+          >
+            {form.formState.isSubmitting ? (
+              <IconLoader2 className="animate-spin" size={24} />
+            ) : (
+              "Login"
+            )}
+          </button>
+        </form>
+      </Form>
     </div>
   );
 }
