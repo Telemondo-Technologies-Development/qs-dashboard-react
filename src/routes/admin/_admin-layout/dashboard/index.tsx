0e7045353d70ec828c3b500f10c9aa9ae5f817dd
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-06-04 22:00:34 +0800

    chore: made AppointmentsSection static
    In this commit I:
    
    - made AppointmentsSection static and part of the index layout

--- src/routes/admin/_admin-layout/dashboard/index.tsx
+++ src/routes/admin/_admin-layout/dashboard/index.tsx
@@ -1,16 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
-import AdminDashboard from './components/admin/-AdminDashboard'
-import StaffDashboard from './components/staff/-StaffDashboard'
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+import AdminDashboard from "./components/admin/-AdminDashboard";
+import StaffDashboard from "./components/staff/-StaffDashboard";
+import AppointmentsSection from "./-AppointmentsSection";
 
-const temp_user = "admin"
+let temp_user = "admin";
 
-export const Route = createFileRoute('/admin/_admin-layout/dashboard/')({
-  component: () => <Dashboard/>
-})
-
+export const Route = createFileRoute("/admin/_admin-layout/dashboard/")({
+  component: () => <Dashboard />,
+});
 
 function Dashboard() {
   return (
-    temp_user==="admin" ? <AdminDashboard/> : <StaffDashboard/>
-  )
+    <div className="flex flex-1 gap-5 px-8 pb-6">
+      {temp_user === "admin" ? <AdminDashboard /> : <StaffDashboard />}
+      <AppointmentsSection />
+    </div>
+  );
 }
