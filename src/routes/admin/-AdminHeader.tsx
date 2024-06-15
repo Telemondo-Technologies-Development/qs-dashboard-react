e6de21ade287e847095b2d9b90064bc0721ed178
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-06-15 17:19:19 +0800

    feat: initial UI of monitor management
    In this commit I:
    
    - setup admin/monitor-management and admin/monitor-management/add-monitor route
    - made the initial static UI of admin/monitor-management and admin/monitor-management/add-monitor
    - added monitor management to navbar
    - added pr-20 (padding right) to AdminHeader
    - added necessary assets for monitor management initial UI

--- src/routes/admin/-AdminHeader.tsx
+++ src/routes/admin/-AdminHeader.tsx
@@ -2,7 +2,7 @@
 
 export default function AdminHeader() {
     return (
-        <div className="flex items-center justify-between px-8 pt-6 pb-3">
+        <div className="flex items-center justify-between px-8 pt-6 pb-3 pr-20">
           <p className="text-xl font-bold"><span className="font-normal">Good Morning,</span> User!</p>
           <div className="flex items-center gap-10">
             <img src="/notification.svg" className="size-5" />
---
+++ src/routes/admin/_admin-layout/monitor-management/-MonitorCard.tsx
@@ -0,0 +1,22 @@
+import MiniMonitorPreview from "./-MiniMonitorPreview";
+
+type MonitorCardProps = {
+  index: number;
+};
+
+export default function MonitorCard({ index }: MonitorCardProps) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 px-6 py-4 rounded-lg size-full bg-main_extra">
+      <p className="font-bold">Monitor {index + 1}</p>
+      <MiniMonitorPreview />
+      <div className="flex justify-center w-full gap-4">
+        <button className="w-[38%] py-2 text-main_primary bg-main_secondary rounded-md">
+          Preview
+        </button>
+        <button className="w-[38%] py-2 text-white bg-main_primary rounded-md">
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+}
