da97da306cbbd87589a369c723f0ce8c7bf26432
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-06-18 12:48:48 +0800

    chore: added mock create countertype in login
    In this commit I:
    
    - added isLoggedIn state to  SampleLogin.tsx and a logout button to logout.
    - added mock create countertype button to create a countertype
    - everything is working

--- src/routes/admin/login/-SampleLogin.tsx
+++ src/routes/admin/login/-SampleLogin.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function SampleLogin() {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect(() => {
+  //   if(!isLoggedIn)
+  // }, [email])
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -25,6 +30,7 @@ export default function SampleLogin() {
             )
             .then((res) => {
               setIsLoading(false);
+              setIsLoggedIn(true);
               setEmail(res.data.data.email);
             })
             .catch((err) => {
@@ -35,12 +41,52 @@ export default function SampleLogin() {
       >
         LOGIN
       </button>
-      {isLoading ? (
-        <p className="text-lg">LOADING...</p>
-      ) : (
+      <button
+        onClick={() => {
+          setIsLoading(true);
+          axios
+            .post("/api/core/user/logout")
+            .then((res) => {
+              setIsLoading(false);
+              setIsLoggedIn(false);
+              setEmail("");
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }}
+        className="px-6 py-4 text-lg border border-black"
+      >
+        LOGOUT
+      </button>
+      {isLoading && <p className="text-lg">LOADING...</p>}
+      {isLoggedIn ? (
         <p className="text-lg">LOGGED IN</p>
+      ) : (
+        <p className="text-lg">NOT LOGGED IN</p>
       )}
-      <p className="text-lg">{email === "" ? "unknown" : email}</p>
+      {email && <p className="text-lg">{email}</p>}
+      {/* <button
+        onClick={() => {
+          setIsLoading(true);
+          axios
+            .post("/api/domain/counter/type", {
+              name: "Accounts",
+              abbrev: "A",
+              status: 0,
+            })
+            .then((res) => {
+              setIsLoading(false);
+              console.log("successfully created\n", res.data);
+            })
+            .catch((err) => {
+              console.log("error in making countertype", err);
+            });
+        }}
+        className="px-6 py-4 text-lg border border-black"
+      >
+        CREATE COUNTERTYPE
+      </button> */}
     </div>
   );
 }
--- src/routes/admin/login/index.tsx
+++ src/routes/admin/login/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import SampleLogin from './-SampleLogin'
 
 export const Route = createFileRoute('/admin/login/')({
-  component: () => <div>Hello /admin/login/!</div>
+  component: () => <SampleLogin/>
 })
