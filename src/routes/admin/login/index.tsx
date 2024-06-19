16d21b6928b7f43e2d43875af0b5524ec9afcc58
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-06-19 15:38:39 +0800

    chore: changed index.tsx to resolve merge conflict in aadmin-login-services-integration branch

--- src/routes/admin/login/index.tsx
+++ src/routes/admin/login/index.tsx
@@ -1,6 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
-import SampleLogin from './-SampleLogin'
+import { createFileRoute } from "@tanstack/react-router";
+import Login from "./components/-Login";
 
-export const Route = createFileRoute('/admin/login/')({
-  component: () => <SampleLogin/>
-})
+export const Route = createFileRoute("/admin/login/")({
+  component: () => (
+    <div className="grid h-screen bg-main_primary place-items-center">
+      <Login />
+    </div>
+  ),
+});
