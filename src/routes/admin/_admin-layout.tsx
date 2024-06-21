b5ccaeac083d9bac1ae49126480269df35de9c30
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-06-21 08:45:43 +0800

    feat: not logged in route protection
    In this commit I:
    
    - implemented not logged in route protection by checking that if user is not logged in, navigate the user to /admin/login. Otherwise, proceed.

--- src/routes/admin/_admin-layout.tsx
+++ src/routes/admin/_admin-layout.tsx
@@ -1,10 +1,11 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Navigate, Outlet, createFileRoute } from "@tanstack/react-router";
 import Navbar from "./-Navbar";
 import Footer from "../kiosk/components/kiosk_dashboard/-Footer";
 import AdminHeader from "./-AdminHeader";
 import AddCategoryDialog from "./_admin-layout/monitor-management/add-monitor/-AddCategoryDialog";
 import { useMonitorManagementStore } from "@/stores/admin/monitorMgmt";
 import PreviewDialog from "./-PreviewDialog";
+import { useUserData } from "@/api/auth";
 
 export const Route = createFileRoute("/admin/_admin-layout")({
   component: () => <AdminLayout />,
@@ -12,19 +13,23 @@ export const Route = createFileRoute("/admin/_admin-layout")({
 
 function AdminLayout() {
   const { showAddCategoryDialog } = useMonitorManagementStore();
-  return (
-    <div className="flex h-screen font-poppins text-main_primary">
-      {showAddCategoryDialog && (
-        <div className="absolute top-0 left-0 z-20 w-screen h-screen bg-black/30"></div>
-      )}
-      <Navbar />
-      <div className="relative flex flex-col flex-1">
-        <PreviewDialog/>
-        <AddCategoryDialog />
-        <AdminHeader />
-        <Outlet />
-        <Footer />
+  const { data: userData } = useUserData();
+  if (userData) {
+    return (
+      <div className="flex h-screen font-poppins text-main_primary">
+        {showAddCategoryDialog && (
+          <div className="absolute top-0 left-0 z-20 w-screen h-screen bg-black/30"></div>
+        )}
+        <Navbar />
+        <div className="relative flex flex-col flex-1">
+          <PreviewDialog />
+          <AddCategoryDialog />
+          <AdminHeader />
+          <Outlet />
+          <Footer />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return <Navigate to="/admin/login" />;
 }
