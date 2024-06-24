624334203a438d9b7eefd8bdfccd5a08b36a4dfe
GABRIEL IBANEZ <occasionalprogrammer@gmail.com>
2024-06-24 18:51:53 +0800

    Closes #62
    feat: implement logout UI and services
    
    in this commit i:
    - implemented the UI
    - integrated the user logout API

--- src/routes/admin/-AdminHeader.tsx
+++ src/routes/admin/-AdminHeader.tsx
@@ -1,7 +1,8 @@
-import { useUserData } from "@/api/auth";
+// import { useUserData } from "@/api/auth";
+import UserDropDown from "./-UserDropDown";
 
 export default function AdminHeader() {
-  const {data: userData, error} = useUserData()
+  // const { data: userData, error } = useUserData();
 
   return (
     <div className="flex items-center justify-between px-8 pt-6 pb-3">
@@ -12,13 +13,7 @@ export default function AdminHeader() {
         <img src="/notification.svg" className="size-5" />
         <div className="flex items-center gap-[10px]">
           <img src="/user-icon.svg" className="size-5" />
-          <p className="text-sm font-bold">
-            {error
-              ? error.message
-              : userData
-                ? `${userData.firstName} ${userData.lastName}`
-                : "Loading..."}
-          </p>
+          <UserDropDown />
         </div>
       </div>
     </div>
